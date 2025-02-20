776b5ab52573f31796365ca18b17d7e57566a166
soongu <naong526@naver.com>
2025-02-20 09:51:35 +0900

    # 9. 회원 엔터티 생성

---
+++ src/main/java/com/study/event/domain/eventUser/entity/EventUser.java
@@ -0,0 +1,42 @@
+package com.study.event.domain.eventUser.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "tbl_event_user")
+public class EventUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ev_user_id")
+    private Long id;
+
+    @Column(name = "ev_user_email", nullable = false, unique = true)
+    private String email;
+
+    // NN을 안거는 이유 : SNS 로그인한 회원, 인증번호만 받고 회원가입을 마무리하지 않은 회원 때문
+    @Column(length = 500)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Role role = Role.COMMON; // 권한
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    // 이메일 인증을 완료했는지 여부
+    @Column(nullable = false)
+    private boolean emailVerified;
+}
---
+++ src/main/java/com/study/event/domain/eventUser/entity/Role.java
@@ -0,0 +1,5 @@
+package com.study.event.domain.eventUser.entity;
+
+public enum Role {
+    COMMON, PREMIUM, ADMIN
+}
