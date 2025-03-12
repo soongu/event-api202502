0070c9125d473b85741f1e97ae717a74975ad9bd
soongu <naong526@naver.com>
2025-03-12 10:14:01 +0900

    # 35. CORS설정에 리액트가 배포된 클라이언트 호스트 추가

--- src/main/java/com/study/event/config/CrossOriginConfig.java
+++ src/main/java/com/study/event/config/CrossOriginConfig.java
@@ -12,6 +12,7 @@ public class CrossOriginConfig implements WebMvcConfigurer {
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:5175",
+            "http://my-event-app-bucket-hong-ver0001.s3-website.ap-northeast-2.amazonaws.com"
     };
 
     @Override
